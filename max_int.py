e074f18665f01e7ecab9416077925b0d45fb98ec
harpamaria <54940438+harpamaria@users.noreply.github.com>
2020-01-30 15:00:27 +0000

    Add files via upload

---
+++ max_int.py
@@ -0,0 +1,15 @@
+north_int = int(input("Number of cars travelling north: "))
+south_int = int(input("Number of cars travelling south: "))
+east_int = int(input("Number of cars travelling east: "))
+west_int = int(input("Number of cars travelling west: "))
+
+sum_of_ = 0
+
+
+"""
+while ((north_int or south_int) > (east_int or west_int)):
+    print("Green light on N/S")
+
+
+print("Green light on E/W")
+print("No cars waiting, the traffic jam has been solved!")
